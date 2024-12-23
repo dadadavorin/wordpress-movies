3fde2afa6eb81e270dbaa35dbc26cda0386eacc6
Davorin Prislin <davorin.prislin@infinum.hr>
2024-12-23 10:59:26 +0100

    Add movies CPT

---
+++ wp-content/themes/movies/src/CustomPostType/MoviesPostType.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * File that holds class for MoviesPostType custom post type registration.
+ *
+ * @package Movies\CustomPostType
+ */
+
+declare(strict_types=1);
+
+namespace Movies\CustomPostType;
+
+use MoviesVendor\EightshiftLibs\CustomPostType\AbstractPostType;
+
+/**
+ * Class MoviesPostType.
+ */
+class MoviesPostType extends AbstractPostType
+{
+	/**
+	 * Post type slug constant.
+	 *
+	 * @var string
+	 */
+	public const POST_TYPE_SLUG = 'movies';
+
+	/**
+	 * URL slug for the custom post type.
+	 *
+	 * @var string
+	 */
+	public const POST_TYPE_URL_SLUG = 'movie';
+
+	/**
+	 * Rest API Endpoint slug constant.
+	 *
+	 * @var string
+	 */
+	public const REST_API_ENDPOINT_SLUG = 'movies';
+
+	/**
+	 * Capability type for projects post type.
+	 *
+	 * @var string
+	 */
+	public const POST_CAPABILITY_TYPE = 'post';
+
+	/**
+	 * Location of menu in sidebar.
+	 *
+	 * @var int
+	 */
+	public const MENU_POSITION = 20;
+
+	/**
+	 * Set menu icon.
+	 *
+	 * @var string
+	 */
+	public const MENU_ICON = 'dashicons-star-filled';
+
+	/**
+	 * Get the slug to use for the Projects custom post type.
+	 *
+	 * @return string Custom post type slug.
+	 */
+	protected function getPostTypeSlug(): string
+	{
+		return self::POST_TYPE_SLUG;
+	}
+
+	/**
+	 * Get the arguments that configure the Projects custom post type.
+	 *
+	 * @return array<mixed> Array of arguments.
+	 */
+	protected function getPostTypeArguments(): array
+	{
+		return [
+			'label' => \esc_html__('Movies', 'productive'),
+			'public' => true,
+			'publicly_queryable' => true,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'query_var' => true,
+			'capability_type' => self::POST_CAPABILITY_TYPE,
+			'has_archive' => false,
+			'rewrite' => ['slug' => static::POST_TYPE_URL_SLUG, 'with_front' => false],
+			'hierarchical' => false,
+			'menu_icon' => static::MENU_ICON,
+			'menu_position' => static::MENU_POSITION,
+			'supports' => ['title', 'editor', 'author', 'thumbnail', 'excerpt', 'revisions'],
+			'show_in_rest' => true,
+			'rest_base' => static::REST_API_ENDPOINT_SLUG,
+		];
+	}
+}
