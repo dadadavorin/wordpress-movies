b79d129bce6c1f40ed25eed7958f19a4db70e3fc
Davorin Prislin <davorin.prislin@infinum.hr>
2024-12-23 16:43:48 +0100

    Allow qoute block only on movie CPT

--- wp-content/themes/movies/src/Blocks/Blocks.php
+++ wp-content/themes/movies/src/Blocks/Blocks.php
@@ -12,6 +12,8 @@ declare(strict_types=1);
 namespace Movies\Blocks;
 
 use MoviesVendor\EightshiftLibs\Blocks\AbstractBlocks;
+use Movies\CustomPostType\MoviesPostType;
+use WP_Block_Editor_Context;
 
 /**
  * Class Blocks
@@ -46,5 +48,66 @@ class Blocks extends AbstractBlocks
 
 		// Output inline css variables.
 		\add_action('wp_footer', [$this, 'outputCssVariablesInline']);
+
+		// Only allow custom built blocks and limit blocks for Movies CPT.
+		\add_filter('allowed_block_types_all', [$this, 'allowOnlyCustomBlocks'], 21, 2);
+	}
+
+	/**
+	 * Only allow our custom blocks in editor, minus the ones listed in:
+	 * $this->blocksDisallowedByDefault().
+	 *
+	 * @param  bool|array<bool|string> $allowedBlocks Array of block type slugs, or boolean to enable/disable all.
+	 * @param WP_Block_Editor_Context $blockEditorContext The post resource data.
+	 *
+	 * @return bool|array<bool|string>
+	 */
+	public function allowOnlyCustomBlocks($allowedBlocks, WP_Block_Editor_Context $blockEditorContext)
+	{
+		// If all blocks are allowed, $allowedBlocks will be === true. If this is the case, we only
+		// wish to allow our custom blocks.
+		if (!\is_array($allowedBlocks)) {
+			$allowedBlocks = \array_merge(
+				(array) $this->getAllBlocksList([], $blockEditorContext),
+			);
+		}
+
+		// Don't allow blocks which are specific to MoviesPostType on other post types.
+		if ($blockEditorContext->post && $blockEditorContext->post->post_type !== MoviesPostType::POST_TYPE_SLUG) {
+			$allowedBlocks = $this->unsetBlocks($allowedBlocks, $this->moviesCptOnlyBlocks());
+		}
+
+		return \is_array($allowedBlocks) ? \array_values($allowedBlocks) : $allowedBlocks;
+	}
+
+	/**
+	 * Removes blocks from $original array
+	 *
+	 * @param  array<bool|string>|bool $original Array of blocks.
+	 * @param array<bool> $toRemove Array of blocks to remove.
+	 *
+	 * @return array<bool|string>|bool
+	 */
+	protected function unsetBlocks($original, array $toRemove)
+	{
+		if (!\is_array($original)) {
+			return $original;
+		}
+
+		return \array_values(\array_filter($original, function ($blockName) use ($toRemove) {
+			return !isset($toRemove[$blockName]);
+		}));
+	}
+
+	/**
+	 * Blocks which are only registered in Movies CPT.
+	 *
+	 * @return array<string, bool>
+	 */
+	protected function moviesCptOnlyBlocks(): array
+	{
+		return [
+			'eightshift-boilerplate/quote' => true,
+		];
 	}
 }
